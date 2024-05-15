45bfffc77d0c952815980ab40f8fff3a6d200822
diamondhandedblkman <mrosier@adobe.com>
2024-05-15 15:21:21 -0700

    blah

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -7,6 +7,8 @@ export default function decorate(block) {
 
   const innerContent = block.children[1].querySelector('div');
   innerContent.setAttribute('class', 'inner-content');
+  innerContent.setAttribute('data-aue-resource', 'urn:fcsconnection:/bs2');
+  innerContent.setAttribute('data-aue-type', 'component');
 
   if (innerContent) {
     const eyebrow = innerContent.querySelector('h3');
